b113f612a1bfaa23264bc6478ecbd8a76cb8935e
Binthosh J <binthosh.kj@calsoftinc.com>
2021-06-04 23:05:34 +0530

    codeRefactor

---
+++ src/main/java/com/calsoft/demoplan/DemoplanApplication.java
@@ -0,0 +1,13 @@
+package com.calsoft.demoplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calsoft/demoplan/DemoplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calsoft.demoplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
